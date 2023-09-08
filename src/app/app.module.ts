4768a3a820c8fdf5730768d8a9ed79d43a316cee
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2023-09-08 10:24:23 +0200

    Added modules, tables with sorting,pagination and filtering functions. Used pipe. Added some bugs to form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,11 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+MatPaginatorModule;
+
 @NgModule({
   declarations: [AppComponent, EmpAddEditComponent],
   imports: [
@@ -35,6 +40,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatSelectModule,
     ReactiveFormsModule,
     HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
